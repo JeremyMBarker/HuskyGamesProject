c0ce4e9672aa77f5ea05dde5b0248d074e6fd217
Jeremy Barker <barker@c28-0112-14.ad.mtu.edu>
2016-09-10 00:05:27 -0400

    Added initial project

---
+++ BulletHellHGD/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+
+	public float speed;
+
+	private Rigidbody2D rb2d;
+	private float playerSpeed;
+
+	void Start()
+	{
+		rb2d = GetComponent<Rigidbody2D> ();
+	}
+
+	void FixedUpdate()
+	{
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+		if (Input.GetKey (KeyCode.LeftShift)) {
+			playerSpeed = speed / (float)3;
+		} else {
+			playerSpeed = speed;
+		}
+		Vector2 movement = new Vector2 (moveHorizontal, moveVertical);
+		rb2d.velocity = (playerSpeed * movement);
+	}
+
+}
