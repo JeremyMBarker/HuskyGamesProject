f061b52584fd4f1ca9b91ec34dcabe42a0fa01eb
GrantBuhl <grantbuhl@gmail.com>
2016-09-16 15:23:14 -0400

    Added in bullets and the ability to shoot them (no collision yet)

---
+++ BulletHellHGD/Assets/Scripts/BulletCollector.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BulletCollector : MonoBehaviour {
+
+	
+    void OnTriggerEnter(Collider other)
+    {
+        Destroy(other.gameObject.transform.parent.gameObject);
+    }
+}
---
+++ BulletHellHGD/Assets/Scripts/PlayerBullet.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerBullet : MonoBehaviour
+{
+
+    // Use this for initialization
+    public float bSpeed;
+    private Rigidbody2D rb2d;
+
+    void Start()
+    {
+
+        rb2d = GetComponent<Rigidbody2D>();
+        rb2d.AddForce(transform.up * bSpeed);
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        
+    }
+}
