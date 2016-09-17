066f29c24dd0dc21f31c4ef14b3b8aabd08be7a7
JeremyMBarker <barker@mtu.edu>
2016-09-17 15:38:57 -0400

    Created background scrolling

---
+++ BulletHellHGD/Assets/Scripts/BackgroundScroller.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BackgroundScroller : MonoBehaviour
+{
+
+	public float scrollSpeed;
+	public float tileSize;
+
+	private Vector2 startPosition;
+
+	void Start ()
+	{
+		startPosition = transform.position;
+	}
+	
+	void Update ()
+	{
+		float newPosition = Mathf.Repeat (Time.time * scrollSpeed, tileSize);
+		transform.position = startPosition + Vector2.down * newPosition;
+	}
+}
