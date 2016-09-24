fde615ffdf65e28217c28a440c8c0f4c5e93e0ad
GrantBuhl <grantbuhl@gmail.com>
2016-09-24 17:37:11 -0400

    Added New Shooting Mechanics From Grant Branch

---
+++ BulletHellHGD/Assets/Scripts/EnemyHit.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyHit : MonoBehaviour {
+
+    public float health;
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.gameObject.tag == "Bullet")
+        {
+            health = health - (other.gameObject.GetComponent<BulletInfo>().bulletDamage);
+        }
+       
+        if(other.gameObject.transform.parent != null) { 
+            Destroy(other.gameObject.transform.parent.gameObject);
+        }
+        else{
+            
+        }
+        if (health <= 0)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
