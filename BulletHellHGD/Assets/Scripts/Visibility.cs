442980c918900d0860d704d88f9611b9c288cc5f
JeremyMBarker <barker@mtu.edu>
2016-10-15 02:48:38 -0400

    Added the ability to pause the game

---
+++ BulletHellHGD/Assets/Scripts/Visibility.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// makes an object visible when paused
+public class Visibility : MonoBehaviour {
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Time.timeScale == 0)
+			GetComponent<Renderer> ().enabled = true;
+		else
+			GetComponent<Renderer> ().enabled = false;
+	}
+}
