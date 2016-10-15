9363a2a2bb7d1a9e9f0e38e3dbf759ac628cb4a6
mkbandro <mkbandro@mtu.edu>
2016-10-15 13:36:06 -0400

    Add files via upload
    Update Score when enemy killed and Update GUI score value

---
+++ BulletHellHGD/EnemyHit.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class EnemyHit : MonoBehaviour {
+
+    public float health;
+    public int scoreValue;
+    public PlayerMovement playerMovement;
+    
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.gameObject.tag == "Bullet")
+        {
+            health = health - (other.gameObject.GetComponent<BulletInfo>().bulletDamage);
+        }
+       
+        if(other.gameObject.transform.parent != null) { 
+            Destroy(other.gameObject.transform.parent.gameObject);
+        }
+        else{
+            
+        }
+        if (health <= 0)
+        {
+            playerMovement.addScore(scoreValue);
+            Destroy(this.gameObject);
+        }
+        
+    }
+}
