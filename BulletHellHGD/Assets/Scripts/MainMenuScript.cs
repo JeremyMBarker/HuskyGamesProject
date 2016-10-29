48c96bfd55a61f1ce19c5f9899cdb6dfff9b71c4
MichaelLGlazier <mglazier@mtu.edu>
2016-10-29 12:32:01 -0400

    Implemented a option menu Has anti aliasing controls and aspect ration controls, the aspect ration controls are still a work in progress however and are disabled at the moment.

--- BulletHellHGD/Assets/Scripts/MainMenuScript.cs
+++ BulletHellHGD/Assets/Scripts/MainMenuScript.cs
@@ -1,24 +1,31 @@
 ﻿using UnityEngine;
 using System.Collections;
-
+using UnityEngine.UI;
 public class MainMenuScript : MonoBehaviour {
 
+	public GameObject playBut;
+	public GameObject optionBut;
+	public GameObject exitBut;
+
+	//hold original position value
+	private Vector3 playPos;
+	private Vector3 optionPos;
+	private Vector3 exitPos;
 	//Resolution res;
 	// Use this for initialization
 	void Start () {
-
-	}
-	void OnGUI(){
-		/*GUI.Box(new Rect(10,10,1000,900), "Loader Menu");
-
-		if(GUI.Button(new Rect(20, 40, 80, 20), "Level 1")){
-			//do thing
-		}
-		//Debug.Log(res.width());
-*/
+		playPos = new Vector3(playBut.transform.position.x, playBut.transform.position.y, playBut.transform.position.z);
+		optionPos = new Vector3(optionBut.transform.position.x, optionBut.transform.position.y, optionBut.transform.position.z);
+		exitPos = new Vector3(exitBut.transform.position.x, exitBut.transform.position.y, exitBut.transform.position.z);
 	}
 	// Update is called once per frame
 	void Update () {
-	
+		if(buttonScript.moving == 1){
+			//move play Button
+			Vector3 translate = new Vector3(playPos.x, playPos.y + 100000, playPos.z);
+			Vector3 origin = new Vector3(playPos.x, playPos.y, playPos.z);
+			GameObject button = GameObject.FindGameObjectWithTag("playButton");
+			button.transform.position =  Vector3.Lerp(origin, translate, 0.01f * Time.deltaTime);
+		}
 	}
 }
--- BulletHellHGD/Assets/Scripts/buttonScript.cs
+++ BulletHellHGD/Assets/Scripts/buttonScript.cs
@@ -1,9 +1,13 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityEngine.SceneManagement;
-
+using UnityEngine.UI;
 public class buttonScript : MonoBehaviour {
-	
+
+	public static int moving = 0;
+	enum ButtonObj {Play, Options, Exit};
+	float right;
+	float posY;
 	// Use this for initialization
 	void Start () {
 		
@@ -15,4 +19,43 @@ public class buttonScript : MonoBehaviour {
 	public void changeScene(int sceneIndex){
 		SceneManager.LoadScene(sceneIndex);
 	}
+
+	public void exitApplicatio(){
+		Application.Quit();
+	}
+
+	public void transition(){
+		if(moving != 1){
+			moving = 1;
+		}
+	}
+
+	public void lockAspect(int level){
+		switch(level){
+			case 1:
+			break;
+		}
+	}
+
+	/**Changes the AA levels
+	 * int level - the level of AA going to
+	 * 0, 2, 4, and 8.
+	 * */
+	public void AA(int level){
+		switch(level){
+		case 0:	
+			QualitySettings.antiAliasing = 0;
+			break;
+		case 1:
+			QualitySettings.antiAliasing = 2;
+			break;
+		case 2:
+			QualitySettings.antiAliasing = 4;
+			break;
+		case 3:
+			QualitySettings.antiAliasing = 8;
+			break;
+		}
+	}
+
 }
