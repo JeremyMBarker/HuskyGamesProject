614f8626ebef1173a0d85687a7dc85ce0a1f330a
Mongoftw <Richardcarpenter25@yahoo.com>
2016-11-29 15:49:29 -0500

    Added in spawning of Enemies
    Enemies spawn in three different locations. Movement is still buggy

--- BulletHellHGD/Assets/Scripts/EnemyScripts/BasicAIMovement.cs
+++ BulletHellHGD/Assets/Scripts/EnemyScripts/BasicAIMovement.cs
@@ -5,13 +5,27 @@ public class BasicAIMovement : MonoBehaviour {
 
     private Vector3 pos1 = new Vector3(-9, 3, 0);
     private Vector3 pos2 = new Vector3(4, 3, 0);
+    private bool started = false;
     public float speed = .1f;
+    
 
 
     // Update is called once per frame
     void Update()
     {
-        transform.position = Vector3.Lerp(pos1, pos2, Mathf.PingPong(Time.time * speed, 1.0f));
+        if (!started)
+        {
+            transform.position = Vector3.Lerp(transform.position, pos2, Mathf.PingPong(Time.time * speed, .5f));
+        }
+        if (transform.position == pos2)
+        {
+            started = true;
+        }
+        if (started)
+        {
+            transform.position = Vector3.Lerp(pos2, pos1, Mathf.PingPong(Time.time * speed, 1.0f));
+        }
+        
     }
 
 }
---
+++ BulletHellHGD/Assets/Scripts/EnemyScripts/EnemySpawning.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemySpawning : MonoBehaviour {
+
+    public PlayerMovement playerHealth;
+    public GameObject enemy;
+    public float spawnTime = 3f;
+    public Transform[] spawnPoints;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        InvokeRepeating("Spawn", spawnTime, spawnTime);
+    }
+
+
+    void Spawn()
+    {
+        if (playerHealth.pHealth <= 0f)
+        {
+            return;
+        }
+        //Find a random index to spawn at between zero and the 
+        //number of spawn points 
+        int spawnPointIndex = Random.Range(0, spawnPoints.Length);
+
+        //creates an instance of the enemy at a random spawn point
+        Instantiate(enemy, spawnPoints[spawnPointIndex].position, spawnPoints[spawnPointIndex].rotation);
+
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
