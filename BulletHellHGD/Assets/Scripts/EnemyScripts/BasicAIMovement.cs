1e5a60db31440fadbed083102273ecae846b515c
Mongoftw <Richardcarpenter25@yahoo.com>
2017-01-29 16:20:51 -0500

    Enemy Hunt's Player
    The enemy moves towards the player and will stop moving when they are a
    set distance away from the player. Still Spawning on screen. WIll fix
    that soon.

--- BulletHellHGD/Assets/Scripts/EnemyScripts/BasicAIMovement.cs
+++ BulletHellHGD/Assets/Scripts/EnemyScripts/BasicAIMovement.cs
@@ -3,15 +3,25 @@ using System.Collections;
 
 public class BasicAIMovement : MonoBehaviour {
 
+    
     private Vector3 pos1 = new Vector3(-9, 3, 0);
     private Vector3 pos2 = new Vector3(4, 3, 0);
     public float speed = .1f;
+    public Transform target;
+    public float MaxDist = 5;
+    public float MinDist = 3;
 
-
+    private void Start()
+    {
+        target = GameObject.Find("Player").transform;
+    }
     // Update is called once per frame
     void Update()
     {
-        transform.position = Vector3.Lerp(pos1, pos2, Mathf.PingPong(Time.time * speed, 1.0f));
+        if(Vector3.Distance(transform.position,target.position) >= MinDist)
+        transform.position += (target.position - transform.position).normalized
+               * speed * Time.deltaTime;
+    
     }
 
 }
